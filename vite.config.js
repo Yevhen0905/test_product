72a45a8a66cd47e12a9674af9683d2de61f0a319
Vittcenko <evgenav83@gmail.com>
2024-06-11 12:02:32 +0300

    fix for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test_product/',
+  base: '/test_product',
   plugins: [vue()],
   resolve: {
     alias: {
